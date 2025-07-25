5398768770202ae92a425d8fdbeafec6fd456c54
willUlisses <williamcavalcante06@gmail.com>
2025-07-25 13:12:37 -0300

    Primeiras interações com swing

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame janela = new JFrame();
 
-        System.out.println("Hello World");
-
+        janela.setBounds(300,100, 900, 700);
+        janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        janela.setVisible(true);
     }
 }
