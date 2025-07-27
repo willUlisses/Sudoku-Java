bb12698fb1336475f5dab1ecc2cb92d8763fdd0b
willUlisses <williamcavalcante06@gmail.com>
2025-07-27 14:58:54 -0300

    startGame method

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
+import Model.Square;
 import Model.SudokuBoard;
 
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -16,8 +16,8 @@ public class App {
     public static void main(String[] args) {
         final Map<String, String> positions = Stream.of(args)
                 .collect(Collectors.toMap(
-                        k -> k.split(";")[0],
-                        v -> v.split(";")[1]
+                        k -> k.split(";")[0], //pega a linha e a coluna (posições como chave)
+                        v -> v.split(";")[1] //pega o expectedContent e se ele é fixo ou não
                 ));
     int option = 0;
     while (true) {
@@ -44,7 +44,26 @@ public class App {
             default -> System.out.println("Opção inválida, tente novamente uma das opções listadas no menu");
         }*/
     }
+    }
 
+    private static void startGame(final Map<String, String> positions) {
+        if (Objects.nonNull(board)) {
+            System.out.println("O jogo já foi iniciado");
+            return;
+        }
 
+        List<List<Square>> squares = new ArrayList<>();
+        for (int i = 0; i < BOARD_LIMIT; i ++) {
+            squares.add(new ArrayList<>());
+            for (int j = 0; j < BOARD_LIMIT; j++){
+                 String squareConfig = positions.get("%s, %s".formatted(i,j));
+                 Integer expectedContent = Integer.parseInt(squareConfig.split(",")[0]);
+                 boolean squareStatus = Boolean.parseBoolean(squareConfig.split(",")[1]);
+                 Square square = new Square(expectedContent, squareStatus);
+                 squares.get(i).add(square);
+            }
+        }
+        board = new SudokuBoard(squares);
+        System.out.println("O jogo foi iniciado com sucesso!");
     }
 }
